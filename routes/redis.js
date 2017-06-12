7a9a4ce00255880cd66aef63472588dd40a9dc22
AdityaT101 <aditya.takale1990@gmail.com>
2017-06-11 22:29:10 -0700

    changes to query page
    Signed-off-by: AdityaT101 <aditya.takale1990@gmail.com>

--- routes/redis.js
+++ routes/redis.js
@@ -7,15 +7,15 @@ var client = require('redis').createClient(6379, 'r1.7m5dyg.ng.0001.use2.cache.a
 exports.RedisInsert = function (req, res) {
 
     //the data is received on server side.It is further unpacked, and inserted in redis
-    var p1 = req.body[0].data1;//p1 is the counter number
-    var p2 = req.body[0].data2;//p2 is the Click number
-    var p3 = req.body[0].data3;//p3 is the timestamp
+    var CounterNumber = req.body[0].data1;//p1 is the counter number
+    var ClickNumber = req.body[0].data2;//p2 is the Click number
+    var TimeStamp = req.body[0].data3;//p3 is the timestamp
 
     //the data is arranged to be sent to redis
-    var p4 = (parseFloat(p3))
+    var TimeStampFinal = (parseFloat(TimeStamp))
 
     /*The data is pushed into redis. We are using ZScore to store the data.*/
-    client.ZADD(p1, p4, p2,  function (err, reply) {
+    client.ZADD(CounterNumber, TimeStampFinal, ClickNumber,  function (err, reply) {
         if (err) {
             res.status(400).send("bad request");
             console.log(err);
